cd26cce0219d44accdc12b15075468c8f62e3f41
Xu Di <xudifsd@gmail.com>
2011-07-21 17:58:56 +0800

    make html interface more convenient, people can click each tag to skim code now.

--- interpreter.h
+++ interpreter.h
@@ -2,7 +2,7 @@
 #define INTERPRETER_H
 #include "cJSON.h"
 
-extern cJSON * interpret_invocation(struct invocation *);
+extern cJSON * interpret_invocation(struct invocation *,char *, char *);
 extern cJSON * interpret_definition(struct definition *);
 extern cJSON * interpret_file(struct file *);
 extern char * interpret(struct file *);
